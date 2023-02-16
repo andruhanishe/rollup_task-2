a8b0a533031a1c0872b4c84d13d2f37064bb780d
Andy <49044317+andruhanishe@users.noreply.github.com>
2023-02-15 22:06:56 -0500

    init

---
+++ build/bundle.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// import './index.css'
+// import JS_IMAGE from './assets/JS.png'
+
+const h1 = document.createElement('h1');
+h1.classList.add('header');
+h1.textContent = 'I love JavaScript';
+
+const JSimage = document.createElement('img');
+JSimage.classList.add('js-image');
+JSimage.src = JS_IMAGE;
+
+document.body.append(h1, JSimage);
